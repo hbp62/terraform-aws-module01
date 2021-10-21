5121605f57bd4b4e150884a0676001bff351e606
Hardik Patel <hardik.p@leantaas.com>
2021-10-20 20:28:18 -0700

    first commit

---
+++ main.tf
@@ -0,0 +1,25 @@
+resource "aws_instance" "foo" {
+  #count = var.instance_count
+  ami           = "ami-087c17d1fe0178315" # us-west-2
+  instance_type = var.instance_type
+  tags = {
+    "Name" = "demo01"#format("demo-%02d",(count.index + 1))
+    
+  }
+
+}
+
+resource "aws_eip" "eip" {
+  instance = aws_instance.foo.id
+  vpc      = true
+}
+
+output "eip" {
+  value = aws_eip.eip.address
+}
+
+
+
+
+
+
---
+++ var.tf
@@ -0,0 +1,7 @@
+variable "instance_type" {
+  description="instance Type"
+  default = "t3.micro"
+}
+variable "instance_count" {
+   default="5"
+}
